21c0a020175859e09d20ee3203968d0686f30727
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-07-22 15:15:30 -0300

    Project created

---
+++ src/main/java/com/estudos/courseNelio/CourseNelioApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.courseNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/courseNelio/CourseNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.courseNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
