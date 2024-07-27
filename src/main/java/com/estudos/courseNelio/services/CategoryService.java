d6e11d985f4a61da83b0735b6093c57944f1282c
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-07-26 22:40:02 -0300

    Category

---
+++ src/main/java/com/estudos/courseNelio/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.estudos.courseNelio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudos.courseNelio.entities.Category;
+import com.estudos.courseNelio.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
