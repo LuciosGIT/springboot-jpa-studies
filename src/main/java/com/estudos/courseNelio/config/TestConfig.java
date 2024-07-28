a1dbf2e3c2a4797bdff72c7c68709bff57e5b16e
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-07-28 19:38:52 -0300

    Payment, one-to-one association

--- src/main/java/com/estudos/courseNelio/config/TestConfig.java
+++ src/main/java/com/estudos/courseNelio/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudos.courseNelio.entities.Category;
 import com.estudos.courseNelio.entities.Order;
 import com.estudos.courseNelio.entities.OrderItem;
+import com.estudos.courseNelio.entities.Payment;
 import com.estudos.courseNelio.entities.Product;
 import com.estudos.courseNelio.entities.User;
 import com.estudos.courseNelio.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
