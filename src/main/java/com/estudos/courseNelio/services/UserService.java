14253508b606279c862915ee651d76f0938e603c
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-07-30 18:32:35 -0300

    Exception handling - update

--- src/main/java/com/estudos/courseNelio/services/UserService.java
+++ src/main/java/com/estudos/courseNelio/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudos.courseNelio.repositories.UserRepository;
 import com.estudos.courseNelio.services.exceptions.DataBaseException;
 import com.estudos.courseNelio.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
